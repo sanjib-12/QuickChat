f5396350830efc9d5af252a2303151f8f2e1461f
sanjib-12 <sanjibbarakoti112@gmail.com>
2024-11-23 22:49:51 -0500

    add the send message functionality

---
+++ client/src/apiCalls/message.js
@@ -0,0 +1,10 @@
+import { axiosInstance } from './index';
+
+export const createNewMessage = async (message) =>{
+   try {
+      const response = await axiosInstance.post('api/message/new-message',message);
+      return response.data;
+   } catch (error) {
+      return error;
+   }
+}
--- client/src/pages/home/components/chat.js
+++ client/src/pages/home/components/chat.js
@@ -1,22 +1,62 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { createNewMessage } from "../../../apiCalls/message";
+import { hideLoader, showLoader } from "../../../redux/loaderSlice";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 
 function ChatArea(){
+   const dispatch = useDispatch();
    const { selectedChat, user } = useSelector(state => state.useReducer);
    const selectedUser = selectedChat.members.find( u => u._id !== user._id);
+   const [message, setMessage] = useState('');
+
+   const sendMessage = async() =>{
+      try {
+         const newMessage = {
+            chatId: selectedChat._id,
+            sender: user._id,
+            text: message
+         }
+         dispatch(showLoader());
+         const response = await createNewMessage(newMessage);
+         dispatch(hideLoader());
+         if(response.success){
+            setMessage('');
+         }
+      } catch (error) {
+         dispatch(hideLoader());
+         toast.error(error.message);
+      }
+   }
    
    return (<>
-            {selectedChat && <div class="app-chat-area">
-               <div class="app-chat-area-header">
+            {selectedChat && <div className="app-chat-area">
+               <div className="app-chat-area-header">
                      { selectedUser.firstname + ' ' + selectedUser.lastname }
                </div>
-               <div>
+               <div className="main-chat-area">
                   
                   CHAT AREA
                </div>
-               <div>
-                     
-                     SEND MESSAGE
+               <div>  
+                  <div className="send-message-div">
+                     <input 
+                        type="text" 
+                        className="send-message-input" 
+                        placeholder="Type a message" 
+                        value = {message}
+                        onChange = {(e)=>{
+                           setMessage(e.target.value);
+                        }}
+                     />
+                     <button 
+                        className="fa fa-paper-plane send-message-btn" 
+                        aria-hidden="true"
+                        onClick = {sendMessage}
+                     >
+                     </button>
+                  </div>
                </div>
             </div>}
          </>)
